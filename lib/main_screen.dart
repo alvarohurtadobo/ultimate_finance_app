9ffd8a0bc521174cfaff1e36f49a108074cb9f38
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2025-09-04 22:09:56 +0200

    [FEA] Cleaned files

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatefulWidget{
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen>{
+class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     throw UnimplementedError();
