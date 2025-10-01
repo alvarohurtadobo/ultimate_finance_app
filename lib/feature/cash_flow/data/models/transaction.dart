b9b84fcd9ae982972cb9c59951f2d41fdd99bed0
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2025-10-01 23:37:01 +0200

    [FIX] Small type, sucessfully upgraded to fvm

--- lib/feature/cash_flow/data/models/transaction.dart
+++ lib/feature/cash_flow/data/models/transaction.dart
@@ -1,5 +1,9 @@
-class Transaction{
-  const Transaction({required this.id, required this.transactionGroupId, required this.amount})
+class Transaction {
+  const Transaction({
+    required this.id,
+    required this.transactionGroupId,
+    required this.amount,
+  });
   final int id;
   final int transactionGroupId;
   final double amount;
