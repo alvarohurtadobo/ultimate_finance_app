4301c5dca13ff3b46090ebf3d038f62f31819c9c
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2025-10-01 23:34:03 +0200

    [FEA] Added transactions model, about to migrate to fvm

---
+++ lib/feature/cash_flow/data/models/transaction.dart
@@ -0,0 +1,6 @@
+class Transaction{
+  const Transaction({required this.id, required this.transactionGroupId, required this.amount})
+  final int id;
+  final int transactionGroupId;
+  final double amount;
+}
