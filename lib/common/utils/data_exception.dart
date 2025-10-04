dbfb328c436fa255f7bd4ac300f34fce890aa523
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2025-10-04 23:51:26 +0200

    [FEA] Permanently removed exceptiondtos

--- lib/common/utils/data_exception.dart
+++ lib/common/utils/data_exception.dart
@@ -1,20 +1,20 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:ultimate_finance_app/common/utils/un_processable_request_dto.dart';
-import 'package:ultimate_finance_app/common/utils/unexpected_error_dto.dart';
+// import 'package:freezed_annotation/freezed_annotation.dart';
+// import 'package:ultimate_finance_app/common/utils/un_processable_request_dto.dart';
+// import 'package:ultimate_finance_app/common/utils/unexpected_error_dto.dart';
 
-part 'data_exception.freezed.dart';
+// part 'data_exception.freezed.dart';
 
-@freezed
-abstract class DataException with _$DataException {
-  const factory DataException.connectionError() = ConnectionError;
-  const factory DataException.timeoutError() = TimeoutError;
-  const factory DataException.unauthorizedError(int codeError) =
-      UnauthorizedError;
-  const factory DataException.customError(String message) = CustomError;
-  const factory DataException.unexpectedError({UnexpectedErrorDto? data}) =
-      UnexpectedError;
-  const factory DataException.unProcessableRequest(
-    int httpCode,
-    UnProcessableRequestDto data,
-  ) = UnProcessableRequest;
-}
+// @freezed
+// abstract class DataException with _$DataException {
+//   const factory DataException.connectionError() = ConnectionError;
+//   const factory DataException.timeoutError() = TimeoutError;
+//   const factory DataException.unauthorizedError(int codeError) =
+//       UnauthorizedError;
+//   const factory DataException.customError(String message) = CustomError;
+//   const factory DataException.unexpectedError({UnexpectedErrorDto? data}) =
+//       UnexpectedError;
+//   const factory DataException.unProcessableRequest(
+//     int httpCode,
+//     UnProcessableRequestDto data,
+//   ) = UnProcessableRequest;
+// }
--- lib/common/utils/domain_exception.dart
+++ lib/common/utils/domain_exception.dart
@@ -1,15 +1,15 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:ultimate_finance_app/common/utils/data_exception.dart';
-import 'package:ultimate_finance_app/common/utils/payload.dart';
+// import 'package:freezed_annotation/freezed_annotation.dart';
+// import 'package:ultimate_finance_app/common/utils/data_exception.dart';
+// import 'package:ultimate_finance_app/common/utils/payload.dart';
 
-part 'domain_exception.freezed.dart'; 
+// part 'domain_exception.freezed.dart'; 
 
-@freezed
-class DomainException with _$DomainException {
-  const factory DomainException({
-    @Default('') String message,
-    @Default(0) int code,
-     required DataException exceptionType,
-    @Default(null) Payload? payload,
-  }) = _DomainException;
-}
+// @freezed
+// class DomainException with _$DomainException {
+//   const factory DomainException({
+//     @Default('') String message,
+//     @Default(0) int code,
+//      required DataException exceptionType,
+//     @Default(null) Payload? payload,
+//   }) = _DomainException;
+// }
