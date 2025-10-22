4b9e00917a5bff18e0725b67d5cfb8aabad33e35
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2025-10-22 22:44:58 +0200

    [FIX] Removed redundant code and comments

--- lib/common/services/remote_config_service.dart
+++ lib/common/services/remote_config_service.dart
@@ -16,31 +16,3 @@ class RemoteConfigService {
   bool get isMaintenance => _remoteConfig.getBool('maintenance');
   String get appVersion => _remoteConfig.getString('version');
 }
-
-// import 'package:firebase_remote_config/firebase_remote_config.dart';
-
-// class RemoteConfigService {
-//   final FirebaseRemoteConfig _remoteConfig = FirebaseRemoteConfig.instance;
-
-//   Future<void> initialize() async {
-//     await _remoteConfig.setConfigSettings(
-//       RemoteConfigSettings(
-//         fetchTimeout: const Duration(seconds: 10),
-//         minimumFetchInterval: const Duration(
-//           hours: 1,
-//         ), // evita fetchs muy frecuentes
-//       ),
-//     );
-
-//     await _remoteConfig.setDefaults({
-//       'welcome_message': 'Bienvenido por defecto',
-//       'show_promo': false,
-//     });
-
-//     await _remoteConfig.fetchAndActivate();
-//   }
-
-//   String get welcomeMessage => _remoteConfig.getString('welcome_message');
-//   String get version => _remoteConfig.getString('version');
-//   bool get showPromo => _remoteConfig.getBool('show_promo');
-// }
--- lib/di/injection_manual.dart
+++ lib/di/injection_manual.dart
@@ -1,14 +1,11 @@
-// lib/di/injection_manual.dart
 import 'package:get_it/get_it.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:ultimate_finance_app/common/repositories/remote_config_bool_repository_impl.dart';
 import 'package:ultimate_finance_app/common/services/remote_config_service.dart';
-// import 'package:ultimate_finance_app/di/injection.dart';
 
 final GetIt getIt = GetIt.instance;
 
 Future<void> configureDependenciesManual() async {
-  // 1) Configurar Remote Config y hacer fetch+activate (equivalente a @preResolve)
   final remoteConfig = FirebaseRemoteConfig.instance;
 
   await remoteConfig.setConfigSettings(RemoteConfigSettings(
@@ -23,21 +20,17 @@ Future<void> configureDependenciesManual() async {
 
   await remoteConfig.fetchAndActivate();
 
-  // 2) Registrar en GetIt
-  // Registramos la instancia de FirebaseRemoteConfig
   getIt.registerSingleton<FirebaseRemoteConfig>(remoteConfig);
 
-  // Registrar el servicio como lazySingleton (se crea a la primera petición)
   getIt.registerLazySingleton<RemoteConfigService>(
     () => RemoteConfigService(remoteConfig),
   );
 
-  // Registrar la implementación del repositorio
   getIt.registerLazySingleton<RemoteConfigBoolRepositoryImpl>(
     () => RemoteConfigBoolRepositoryImpl(remoteConfig),
   );
 
-  // Si tienes la interfaz, registra la implementación bajo la interfaz:
+  // In case of GUI
   // getIt.registerLazySingleton<RemoteConfigBoolRepository>(
   //   () => RemoteConfigBoolRepositoryImpl(remoteConfig),
   // );
